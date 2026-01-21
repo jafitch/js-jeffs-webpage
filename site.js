de16dbcfa0b477938b040496967c118c2ca8d7d2
Jeff Fitch <jfitch360@gmail.com>
2026-01-21 12:54:29 -0600

    Exercise 2 part 1-underline 'fork this project on GitHub' through the site.js

--- site.js
+++ site.js
@@ -1,2 +1,13 @@
-const mainContentSection=document.querySelector('h2')
-mainContentSection.style.color='red'
+//Exercise 1
+// Select the main content section and change its text color to red 
+const mainContentSection = document.querySelector('h2')
+mainContentSection.style.color = 'red'
+// const activeElements = document.querySelectorAll('.active')
+// add border
+// activeElements.forEach(el => el.style.border = '1px solid red')
+
+//Exercise 2
+// Select the 'Fork this project on GitHub' and underline it
+const footerP=document.querySelector('footer p')
+footerP.style.textDecoration='underline'
+
